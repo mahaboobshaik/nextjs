b98c9b856b309ff3a07a9c1c19967602a0e0cc3f
mahaboob Basha Shaik <mahaboob.basha@dolcera.com>
2019-11-23 13:01:02 +0530

    * added getInitialProps module

--- actions/index.js
+++ actions/index.js
@@ -33,6 +33,7 @@ export const getMovies = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(MOVIE_DATA);
+            // reject('Cannot fetch data');
         }, 2000)
     })
     
